a200fe96063eb957230547a9629f478858822a63
Long Nguyen <18nguyenl@gmail.com>
2019-10-05 02:47:44 +0000

    Add summaries to past events

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const { JSDOM } = require("jsdom");
 
 module.exports = function(eleventyConfig) {
     // Enable assets
@@ -14,5 +15,21 @@ module.exports = function(eleventyConfig) {
     // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
+
+    eleventyConfig.addNunjucksShortcode("meetingSummary", function(markdown, url) {
+        // Parse strings as HTML
+        const dom = new JSDOM(markdown);
+
+        let firstParagraph = dom.window.document.querySelector("p").textContent;
+
+        // Read first 50 words and parse only to the last sentence to return
+        let regExp = /^(\w+\W+?){50}/g
+
+        if (regExp.test(firstParagraph)) {
+            firstParagraph = firstParagraph.slice(0, firstParagraph.lastIndexOf(".", regExp.lastIndex)) + `. <a href=${url}>read more…</a>`;
+        }
+
+        return `<p>${firstParagraph}</p>`;
     });
 }
